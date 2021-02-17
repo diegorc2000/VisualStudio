ebb6a18551f1a7f67504455f56d080f174b59bc4
Diego <diegorc2000@icloud.com>
2021-02-17 13:59:52 +0100

    primer commit

---
+++ js/matematicas.js
@@ -0,0 +1,24 @@
+
+/*
+La funcion suma tiene 2 parametros de entrada y uno de salida con el 
+resultado
+*/
+function sumar(a,b){
+  var resultado = a + b;
+  return resultado;
+}
+
+function resta(a,b){
+  var resultado = a - b;
+  return resultado;
+}
+
+function multiplicacion(a,b){
+  var resultado = a * b;
+  return resultado;
+}
+
+function division(a,b){
+  var resultado = a / b;
+  return resultado;
+}
